ef16000e64e16b39a3e2c3236779e5ea0edf3478
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-01-30 13:02:35 +1100

    fix(publication): enabled publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	id("com.android.library")
 	id("kotlin-android")
 	id("org.jetbrains.dokka") version "1.9.10"
-//	id("maven-publish")
+	id("maven-publish")
 }
 
 android {
@@ -41,22 +41,22 @@ dependencies {
 	compileOnly("org.firstinspires.ftc:FtcCommon:9.0.1")
 }
 
-//publishing {
-//	publications {
-//		register<MavenPublication>("release") {
-//			groupId = "dev.frozenmilk.dairy"
-//			artifactId = "Sinister"
-//			version = "v0.0.0"
-//
-//			afterEvaluate {
-//				from(components["release"])
-//			}
-//		}
-//	}
-//	repositories {
-//		maven {
-//			name = "Core"
-//			url = uri("${project.buildDir}/release")
-//		}
-//	}
-//}
+publishing {
+	publications {
+		register<MavenPublication>("release") {
+			groupId = "dev.frozenmilk.dairy"
+			artifactId = "Sinister"
+			version = "v0.0.0"
+
+			afterEvaluate {
+				from(components["release"])
+			}
+		}
+	}
+	repositories {
+		maven {
+			name = "Sinister"
+			url = uri("${project.buildDir}/release")
+		}
+	}
+}
