810f2c619ae3eefaa08424181de56468749a80e5
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-03-26 01:56:08 +1100

    feat: filters now have an init method

--- src/main/kotlin/dev/frozenmilk/sinister/SinisterFilter.kt
+++ src/main/kotlin/dev/frozenmilk/sinister/SinisterFilter.kt
@@ -6,11 +6,12 @@ import dev.frozenmilk.sinister.targeting.SearchTarget
  * static implementations of this will get invoked at runtime
  */
 @Preload
+@JvmDefaultWithCompatibility
 interface SinisterFilter {
 	/**
 	 * items that should be ignored
 	 */
 	val targets: SearchTarget
-
+	fun init()
 	fun filter(clazz: Class<*>)
 }
