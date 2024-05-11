ff2b3d468da2a0382fb0282c9520872b2bf30893
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-05-12 03:40:57 +1000

    fix: added teamcode back to NarrowSearch
    - in the past, the ignoring of the the sdk also ignored teamcode,
      incorrectly

--- src/main/kotlin/dev/frozenmilk/sinister/targeting/NarrowSearch.kt
+++ src/main/kotlin/dev/frozenmilk/sinister/targeting/NarrowSearch.kt
@@ -6,6 +6,8 @@ package dev.frozenmilk.sinister.targeting
 open class NarrowSearch : WideSearch() {
 	init {
 		exclude("org.firstinspires")
+		// need to re-include team code
+		include("org.firstinspires.ftc.teamcode")
 		exclude("com.qualcomm")
 	}
 }
